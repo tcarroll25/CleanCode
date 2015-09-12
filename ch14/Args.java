701ecb01639c240193a48ff9532bc085927741e6
tyc1031 <tyc1031@gmail.com>
2015-09-12 15:35:48 -0400

    add argument marshaler skeleton

--- ch14/Args.java
+++ ch14/Args.java
@@ -256,4 +256,23 @@ public class Args {
         
     private class ArgsException extends Exception {
     }
+
+    private class ArgumentMarshaler {
+        private boolean booleanValue = false;
+
+        public void setBoolean(boolean value) {
+            booleanValue = value;
+        }
+
+        public boolean getBoolean() {return booleanValue;}
+    }
+
+    private class BooleanArgumentMarshaler extends ArgumentMarshaler {
+    }
+
+    private class StringArgumentMarshaler extends ArgumentMarshaler {
+    }
+
+    private class IntegerArgumentMarshaler extends ArgumentMarshaler {
+    }
 }
